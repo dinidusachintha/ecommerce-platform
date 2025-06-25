24913c635015d7adc18c948e9ad1f3501a15d262
dinidusachintha <dinidusachintha7@gmail.com>
2025-06-26 01:21:35 +0530

    create title component

---
+++ src/Components/latestCollection/latestCollection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const latestCollection = () => {
+
+    const {products} = useContext(ShopContext);
+
+    console.log(products);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default latestCollection
