3cfa5d3dc150d95e93977c4c0300a86dd701693e
dinidusachintha <dinidusachintha7@gmail.com>
2025-06-30 22:52:00 +0530

    add product model

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,17 +4,11 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: true },
-  category: {
-    type: String,
-    required: true,
-    enum: ['women', 'men', 'kids']
-  },
+  category: { type: String, required: true, enum: ['women', 'men', 'kids'] },
   colors: [{ type: String }],
   sizes: [{ type: String }],
   images: [{ type: String }],
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Added for createProduct
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Product', productSchema);
